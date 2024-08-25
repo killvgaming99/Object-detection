bcf669cbac8244ad51bc805a920887ca85fcf4fa
killvgaming99 <160217243+killvgaming99@users.noreply.github.com>
2024-08-25 10:36:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function scrollToAbout() {
+    document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+}
+
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
     
